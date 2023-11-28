1a72e88c16bceaf78ac043751d20a25833f27703
Gabriel Reliquias <125402939+relicote@users.noreply.github.com>
2023-11-28 14:32:51 -0300

    Update engine.js
    criado a função de listener mas ainda sem validação do click de usuário

--- src/scripts/engine.js
+++ src/scripts/engine.js
@@ -1,8 +1,26 @@
+// setando o estado inicial do sistema recebendo as variaveis e constantes do projeto por classe e ID.
+
 const state = {
     view: {
-        squares: document.querySelectorAll(".square")
-        enemy: document.querySelector(".enemy")
+        squares: document.querySelectorAll(".square"),
+        enemy: document.querySelector(".enemy"),
+        timeLeft: document.querySelector("#time-left"),
+        score: document.querySelector("#score"),
     },
     values: {},
 };
 
+// Recebendo a ação de click e armazenando para ser reutilizada
+function addListenerHitBox(){
+    state.view.squares.forEach((square)=>{
+        // verificando qual ID está sendo escolhida
+       
+    });
+}
+
+// criando função de start
+
+function initialize(){}
+
+// iniciando o callback da função
+initialize();
